53c2504248c68ef494a3fa97e3ff5dc4a597aecf
Admin <Admin@DESKTOP-GJD8B8L>
2019-10-30 16:49:49 +0530

    initial commit

---
+++ desingPattern/src/desingPattern/test/Singleton.java
@@ -0,0 +1,16 @@
+package desingPattern.test;
+
+public class Singleton {
+    private static Singleton obj;
+
+    private Singleton() {
+    }
+    
+    public static Singleton getInstance() {
+        if(obj==null)
+            obj=new Singleton();
+        return obj;
+    }
+    
+
+}
---
+++ desingPattern/src/desingPattern/test/Test.java
@@ -0,0 +1,10 @@
+package desingPattern.test;
+
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        Singleton.getInstance();
+    }
+
+}
